c61a71aad67c7d2fe3b3001027f5082ee5e7fe25
Jack McKinnon <jackmckinnon13@hotmail.com>
2022-06-04 12:00:20 -0400

    Adding Module 13 Challenge

--- static/js/challenge_logic.js
+++ static/js/challenge_logic.js
@@ -15,7 +15,7 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 	accessToken: API_KEY
 });
 
-// Create a 3rd dark tile layer 
+// Making 3rd dark tile layer 
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API Key
-const API_KEY = "pk.eyJ1IjoibWNramFjayIsImEiOiJjbDNrcmh4OGYwZGRzM2NxYWhvbjA1bWpnIn0.PBK_WXa17Mz8f1UM2jPBhQ";
+var API_KEY = "pk.eyJ1IjoibWNramFjayIsImEiOiJjbDNrcmh4OGYwZGRzM2NxYWhvbjA1bWpnIn0.PBK_WXa17Mz8f1UM2jPBhQ";
